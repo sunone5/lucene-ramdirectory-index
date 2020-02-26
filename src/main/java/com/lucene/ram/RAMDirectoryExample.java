15c9b9e186e015ec123340a53371d8fa68d1bbd2
intel <intel@master>
2020-02-26 19:02:10 +0530

    second commit

---
+++ src/main/java/com/lucene/ram/RAMDirectoryExample.java
@@ -0,0 +1,154 @@
+package com.lucene.ram;
+
+import java.io.IOException;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.RAMDirectory;
+
+public class RAMDirectoryExample {
+
+	public RAMDirectoryExample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	static void writeIndex(RAMDirectory ramDir, Analyzer analyzer) {
+		try {
+			// IndexWriter Configuration
+			IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+			iwc.setOpenMode(OpenMode.CREATE);
+
+			// IndexWriter writes new index files to the directory
+			IndexWriter writer = new IndexWriter(ramDir, iwc);
+
+			// Create some docs with name and content
+			indexDoc(writer, "document-1", "hello world");
+			indexDoc(writer, "document-2", "hello happy world");
+			indexDoc(writer, "document-3", "hello happy world");
+			indexDoc(writer, "document-4", "hello hello world");
+
+			// don't forget to close the writer
+			writer.close();
+		} catch (IOException e) {
+			// Any error goes here
+			e.printStackTrace();
+		}
+	}
+
+	static void indexDoc(IndexWriter writer, String name, String content) throws IOException {
+		Document doc = new Document();
+		doc.add(new TextField("name", name, Store.YES));
+		doc.add(new TextField("content", content, Store.YES));
+		writer.addDocument(doc);
+	}
+
+	static void searchIndex(RAMDirectory ramDir, Analyzer analyzer) {
+		IndexReader reader = null;
+		try {
+			// Create Reader
+			reader = DirectoryReader.open(ramDir);
+
+			// Create index searcher
+			IndexSearcher searcher = new IndexSearcher(reader);
+
+			// Build query
+			QueryParser qp = new QueryParser("content", analyzer);
+			Query query = qp.parse("happy");
+
+			// Search the index
+			TopDocs foundDocs = searcher.search(query, 10);
+
+			// Total found documents
+			System.out.println("Total Results :: " + foundDocs.totalHits);
+
+			// Let's print found doc names and their content along with score
+			for (ScoreDoc sd : foundDocs.scoreDocs) {
+				Document d = searcher.doc(sd.doc);
+				System.out.println("Document Number : " + sd.doc +" :: Document Name : " + d.get("name") + "  :: Content : " + d.get("content")
+						+ "  :: Score : " + sd.score);
+			}
+			// don't forget to close the reader
+			reader.close();
+		} catch (IOException e) {
+			// Any error goes here
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	static void readIndex_Get_Documents(RAMDirectory ramDir) {
+		IndexReader reader = null;
+		try {
+			// Create Reader
+			reader = DirectoryReader.open(ramDir);
+
+			// Create index searcher
+			IndexSearcher searcher = new IndexSearcher(reader);
+
+			
+			int maxDoc = reader.maxDoc();
+			for (int i=0; i <maxDoc ; i++)
+			{
+			     Document d = reader.document(i);
+			     
+			     //Method 1
+			     System.out.println(""+d.getFields().iterator().next().stringValue());
+			     
+			     //Method 2
+			     System.out.println(""+d.iterator().next().stringValue());
+			     
+			     //Method 1 for get term list
+			     //System.out.println(""+d.getField("content").stringValue());
+			     
+			     String[] vl = searcher.doc(i).getValues("content");
+			     
+			     for(int k=0; k < vl.length; k++) {
+			    	 System.out.println(""+vl[k].toString());
+			     }
+			     
+			     
+			     //System.out.println(""+d.getFields("content").toString());
+			}
+			
+			// don't forget to close the reader
+			reader.close();
+		} catch (IOException e) {
+			// Any error goes here
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		// Create RAMDirectory instance
+		RAMDirectory ramDir = new RAMDirectory();
+
+		// Builds an analyzer with the default stop words
+		Analyzer analyzer = new StandardAnalyzer();
+
+		// Write some docs to RAMDirectory
+		writeIndex(ramDir, analyzer);
+
+		// Search indexed docs in RAMDirectory
+		searchIndex(ramDir, analyzer);
+		
+		//read Index
+		readIndex_Get_Documents(ramDir);
+	}
+
+}
